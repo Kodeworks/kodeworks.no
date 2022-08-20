b1ffc8585a9ff180c1acae4590083c68b86d0404
Tri Nguyen <mail@trimn.net>
2022-08-20 09:15:45 +0200

    Add Norwegian translation for project page

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,20 +1,25 @@
 import ProjectTile from '../../components/ProjectTile';
 
-import { useClipContent } from '../../context/ClipContentContext';
 import api from '../../api';
+import dictionary from '../dict';
+import { useClipContent } from '../../context/ClipContentContext';
+import { useTranslation } from '../../utils/useTranslation';
 
 export default function Projects(): JSX.Element {
   useClipContent(true);
+  const { t } = useTranslation(dictionary);
 
   return (
     <div id="page-projects" className="theme-dark">
       <header>
-        <h1 className="section-header-headline">Our Projects</h1>
+        <h1 className="section-header-headline">{t('what')}</h1>
 
-        <p>By adopting our value oriented approach we have carried out projects with and for clients of varying size, and across a wide spectrum of domains. Below you can find the types of solutions we have developed for our customers.</p>
+        <p>{t('what_description')}</p>
       </header>
 
-      { api.projects.getProjects().map((project) => <ProjectTile key={ project.name } project={ project }/>)}
+      {api.projects.getProjects().map((project) => (
+        <ProjectTile key={project.name} project={project} />
+      ))}
     </div>
   );
 }
