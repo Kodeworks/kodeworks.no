94ce2fcd232262d6266172d0f7a484a40c499a69
jonasmyrenmo <jonasmo1990@gmail.com>
2023-05-12 16:12:42 +0200

    Added schema and sanity api method for job description

---
+++ studio/schemas/documents/job-description.ts
@@ -0,0 +1,37 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'job-description',
+  title: 'Job description',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Job title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+      },
+    }),
+    defineField({
+      name: 'label',
+      title: 'Location label',
+      type: 'string'
+    }),
+    defineField({
+      name: 'subtitle',
+      title: 'Subtitle',
+      type: 'string'
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'string'
+    }),
+  ],
+})
--- web/src/lib/sanity-api.ts
+++ web/src/lib/sanity-api.ts
@@ -24,3 +24,16 @@ export const getPeople = () =>
   client.fetch<Person>(
     '*[_type == "people" && !(_id in path(\'drafts.**\'))]{firstName, lastName, email, projects[] -> {name, slug}, socials, "imageUrl": image.asset->url}'
   );
+
+export interface JobDescription {
+  title: string;
+  slug: Slug;
+  label: string;
+  subtitle: string;
+  description: string;
+  sections: Array<ManualSection>;
+}
+export const getJobDescriptions = () =>
+  client.fetch<JobDescription>(
+    `*[_type == "job-description"][0]{ title, slug, label, subtitle, description}`
+  );
