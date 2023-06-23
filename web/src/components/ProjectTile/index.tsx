a182103a2c4e41102e267095d4144e6da4354d7e
Thomas Wold <thmwld@gmail.com>
2023-06-23 10:35:25 +0200

    Project page

--- web/src/components/ProjectTile/index.tsx
+++ web/src/components/ProjectTile/index.tsx
@@ -5,63 +5,46 @@ import { fmt, getLocale } from '../../utils/useTranslation';
 
 import Button from '../../components/Button';
 
-import style from './projecttile.module.css';
-
 interface Prop {
   project: Project;
+  reverse: boolean
 }
 
-export default function ProjectTile({ project }: Prop): JSX.Element {
+export default function ProjectTile({ project, reverse }: Prop): JSX.Element {
   const locale = getLocale(useRouter());
 
   return (
-    <article className={style['project']}>
-      <div className={style['projecttile-image-container']}>
+      <article className='grid grid-cols-[1fr] lg:grid-cols-[repeat(2,1fr)] gap-6 items-start mt-24'>
         <img
-          className={style['project-tile--image']}
+          className={`${reverse ? 'lg:order-last' : "order-first"}`}
+          style={{aspectRatio: '16/9', maxHeight: '400px', maxWidth: '100%'}}
           src={'/static/photos/projects/' + project.image}
-          width="1200"
-          height="1200"
           alt={fmt(project.name, locale!)}
         />
-      </div>
+        <div className='w-9/12'>
+          <h3>{fmt(project.name, locale!)}</h3>
+          <p>{fmt(project.description, locale!)}</p>
 
-      <div className={style['projecttile-content']}>
-        <h3>{fmt(project.name, locale!)}</h3>
-        <p>{fmt(project.description, locale!)}</p>
+          {project.published ? (
+            <p>
+              <Button appearance={Button.appearances.DarkNoPadding} href={`input/${project.urlName}`}>
+                Les mer om prosjektet
+              </Button>
+            </p>
+          ) : null}
 
-        {project.published ? (
-          <p>
-            <Button appearance={Button.appearances.DarkNoPadding} href={`input/${project.urlName}`}>
-              Les mer om prosjektet
-            </Button>
-          </p>
-        ) : null}
+          {project.technologies ? (
+            <div className='text-[#999] text-sm'>
+              <h5>Teknologier</h5>
 
-        {project.output ? (
-          <div className={style['projecttile-technologies']}>
-            <h5>Verdi</h5>
-
-            <ul className={style['projecttile-list']}>
-              {project.output?.map((output, index) => (
-                <li key={`${index}`}>{output}</li>
-              ))}
-            </ul>
-          </div>
-        ) : null}
-
-        {project.technologies ? (
-          <div className={style['projecttile-technologies']}>
-            <h5>Teknologier</h5>
-
-            <ul className={style['projecttile-list']}>
-              {project.technologies?.map((tech, index) => (
-                <li key={`${index}`}>{tech}</li>
-              ))}
-            </ul>
-          </div>
-        ) : null}
-      </div>
-    </article>
+              <div className='flex flex-wrap  text-sm gap-4'>
+                {project.technologies?.map((tech, index) => (
+                  <div key={`${index}`} className="border-2 border-solid border-[#B6FF9E] py-1 px-2">{tech}</div>
+                ))}
+              </div>
+            </div>
+          ) : null}
+        </div>
+      </article>
   );
 }
