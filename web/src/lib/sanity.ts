169eb5d5af0041279cfb0de3af9e0f01548c2041
Eirik Baug <eirik.baug@ruter.no>
2023-09-01 14:42:14 +0200

    fix type projects to list

--- web/src/lib/sanity.ts
+++ web/src/lib/sanity.ts
@@ -22,12 +22,12 @@ export const getStaffManual = () =>
   );
 
 export const getPeople = () =>
-  client.fetch<Person>(
+  client.fetch<Person[]>(
     '*[_type == "people" && !(_id in path(\'drafts.**\'))]{firstName, lastName, email, projects[] -> {name, slug}, socials, "imageUrl": image.asset->url}'
   );
 
 export const getProjects = () =>
-  client.fetch<ProjectV2>(
+  client.fetch<ProjectV2[]>(
     '*[_type == "project" && !(_id in path(\'drafts.**\'))]{name, description, "slug":slug.current, "imageUrl": image.asset->url, technologies[]}'
   );
 
