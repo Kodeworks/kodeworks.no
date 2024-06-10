249d98250ddd2d312e11e07d654a3f2bcb143876
Sami Lazreg <xcitic@gmail.com>
2024-06-10 13:42:02 +0200

    Landing page v1 complete

--- web/src/components/Landing/Kunder/index.tsx
+++ web/src/components/Landing/Kunder/index.tsx
@@ -1,21 +1,16 @@
 import Image from 'next/image';
 import { logos } from '../../../utils/kunderLogos';
 
-const imageStyle: React.CSSProperties = {
-  height: 'auto',
-  width: '150px',
-};
-
 const Kunder = () => {
 
   return (
     <div className="flex flex-col section-padding">
       <h2 className="section-title">Våre Kunder</h2>
-      <div className="flex flex-row flex-wrap items-center lg:justify-start justify-center gap-x-12 gap-y-12">
+      <div className="flex flex-row flex-wrap items-center lg:justify-start justify-start xl:gap-x-12 xl:gap-y-12 lg:gap-x-6 lg:gap-y-6 gap-x-4 gap-y-4">
         {logos.map((logo, index) => {
           return (
             <div key={index} className="logo">
-              <Image src={logo.src} alt={logo.alt} width={140} height={30} style={imageStyle} />
+              <Image src={logo.src} alt={logo.alt} width={140} height={30} className="lg:w-[150px] w-[100px] h-auto" />
             </div>
           );
         })}
--- web/src/components/Landing/Oppdrag/index.tsx
+++ web/src/components/Landing/Oppdrag/index.tsx
@@ -4,29 +4,30 @@ const Oppdrag = () => {
     return (
         <div className="flex flex-col section-padding">
             <div className="flex md:flex-row flex-col w-full justify-start gap-8">
-                <div className="self-start">
-                    <video src="/static/videos/david.mov" autoPlay loop muted className="h-[600px] object-fit"></video>
-                </div>
-                <div className="flex flex-col justify-between w-full">
-                    <div className="self-end">
+                {/* Text section initially placed first but will move based on order in CSS */}
+                <div className="flex flex-col justify-between w-full md:order-2 order-1">
+                    <div className="mb-8 md:mb-0"> {/* Added margin-bottom for mobile layout */}
                         <p className="text-4xl">
                             Har du et system du trenger hjelp med?
                             <br></br>
                             David er klar for å bistå deg. 
                         </p>
-                        
                     </div>
-                    <div className="flex flex-col self-start">
+                    <div className="flex flex-col">
                         <h3>David Vimo Garner</h3>
                         <p>david@kodeworks.no</p>
                         <p>99 49 99 00</p>
                         <div>
                             <Link href="/#kontaktoss">
-                                <button className="button xl:px-8 px-6 py-3 border-none" >Kontakt oss</button>
+                                <button className="button xl:px-8 px-6 py-3 border-none">Kontakt oss</button>
                             </Link>
                         </div>
                     </div>
-                    
+                </div>
+
+                {/* Video section */}
+                <div className="self-start md:order-1 order-2">
+                    <video src="/static/videos/david.mov" autoPlay loop muted className="h-[600px] object-cover w-full md:w-auto"></video>
                 </div>
             </div>
         </div>
