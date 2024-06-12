f14577c41e693ad9eae9da9129fdaf4ea07b16b0
Sami Lazreg <xcitic@gmail.com>
2024-06-12 12:41:58 +0200

    Mobile versjon prosjekter

--- web/src/components/ProjectTile/index.tsx
+++ web/src/components/ProjectTile/index.tsx
@@ -12,17 +12,17 @@ export default function ProjectTile({ project, reverse }: Prop): JSX.Element {
   const locale = getLocale(useRouter());
 
   return (
-    <article className={`${reverse ? 'justify-end' : ''} flex lg:flex-row flex-col gap-[44px]`}>
+    <article className={`${reverse ? 'justify-end' : ''} flex lg:flex-row flex-col items-center gap-[44px]`}>
       <img
-        className={`${reverse ? 'lg:order-last' : 'order-first'}`}
-        style={{ aspectRatio: '16/9', maxHeight: '300px', width: '30%' }}
+        className={`${reverse ? 'lg:order-last' : 'order-first'} lg:w-[30%] w-full`}
+        style={{ aspectRatio: '16/9', maxHeight: '300px'}}
         src={project.imageUrl}
         alt={fmt(project.name, locale!)}
       />
-      <div className="lg:w-1/3 md:w-1/2 w-full flex flex-col justify-between">
+      <div className="lg:w-1/3 w-full flex flex-col justify-between">
         <div className="flex flex-col gap-4">
-          <h3>{project.name}</h3>
-          {project.description && <p>{fmt(project.description, locale!)}</p>}
+          <h3 className="section-title">{project.name}</h3>
+          {project.description && <p className="section-text">{fmt(project.description, locale!)}</p>}
 
           {project.technologies ? (
             <div className="flex flex-wrap text-sm gap-4">
@@ -40,7 +40,7 @@ export default function ProjectTile({ project, reverse }: Prop): JSX.Element {
 
         {project.content && (
           <div className="mt-4">
-            <Link href={`prosjekter/${project.slug}`} className="green-link text-base">
+            <Link href={`prosjekter/${project.slug}`} className="green-link text-lg">
               Les mer om prosjektet
             </Link>
           </div>
