acc0923f1b3d7b9faf547233fbe37be68cf30811
Sami Lazreg <xcitic@gmail.com>
2024-06-13 14:07:44 +0200

    Prosjekt side farger

--- web/src/components/ProjectHeader/index.tsx
+++ web/src/components/ProjectHeader/index.tsx
@@ -2,8 +2,6 @@ import { useRouter } from 'next/router';
 
 import { Project } from '../../types';
 import { fmt, getLocale } from '../../utils/useTranslation';
-
-import style from './projectheader.module.css';
 import TechList from '../TechList';
 
 interface Prop {
@@ -14,16 +12,11 @@ export default function ProjectHeader({ project }: Prop): JSX.Element {
   const locale = getLocale(useRouter());
   return (
     <header className="flex flex-col justify-start lg:mt-52 mt-28 lg:mb-16 mb-12">
-        <h1 className="mb-8">{project.name}</h1>
-        <div className="lg:w-1/2 w-full">
-          {project.description &&
-            <p className="mb-3">{fmt(project.description, locale!)}</p>
-          }
-
-          {project.technologies ? (
-              <TechList technologies={project.technologies} />
-            ) : null}
-        </div>
+      <h1 className="mb-8">{project.name}</h1>
+      <div className="lg:w-1/2 w-full">
+        {project.description && <p className="mb-3">{fmt(project.description, locale!)}</p>}
+        {project.technologies && <TechList technologies={project.technologies} />}
+      </div>
     </header>
   );
 }
