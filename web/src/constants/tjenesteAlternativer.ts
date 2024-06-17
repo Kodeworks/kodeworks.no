464b25d22f9f4615cb1d14c1921c25376c557489
Sami Lazreg <xcitic@gmail.com>
2024-06-17 13:27:13 +0200

    Tekst for Utvikling tjenesten

--- web/src/constants/tjenesteAlternativer.ts
+++ web/src/constants/tjenesteAlternativer.ts
@@ -2,23 +2,24 @@ export const tjenesteAlternativer = [
     {
         tittel: 'Utvikling',
         slug: 'utvikling',
-        tekst: 'Innleie av konsulenter for å dekke deres kapasitetsbehov for nøkkelkompetanse og rask verdiskapning.',
+        // tekst: 'Innleie av konsulenter for å dekke deres kapasitetsbehov for nøkkelkompetanse og rask verdiskapning.',
+        tekst: "KodeWorks leverer ekspert konsulenter som dekker alt fra nyutvikling til vedlikehold av eksisterende IT systemer.",
         bilde: '/static/photos/tjenester/kodeworks-tjenester1.png',
         seksjoner: [
             {
                 bilde: '/static/photos/tjenester/kodeworks-tjenester1.png',
-                tittel: 'Utvikling',
-                tekst: 'Vi utvikler skreddersydde løsninger for våre kunder, fra enkle nettsider til komplekse applikasjoner. Vi har bred kompetanse innenfor flere teknologier og plattformer, og vi er opptatt av å levere løsninger som er skalerbare, sikre og brukervennlige.',
+                tittel: 'Erfarne konsulenter',
+                tekst: 'Våre konsulenter bringer enestående ekspertise og omfattende erfaring fra ulike teknologier og bransjer. Vi leverer sikre, effektive og bærekraftige løsninger som er skreddersydd for å møte dine behov.',
             },
             {
                 bilde: '/static/photos/tjenester/kodeworks-tjenester2.png',
-                tittel: 'Design',
-                tekst: 'Vi designer brukervennlige og estetisk tiltalende løsninger som',
+                tittel: 'Teknologisk kompetanse',
+                tekst: 'Vår styrke er å tilpasse oss ulike prosjekter og bransjer, med dyp forståelse av teknologiens potensial for forretningsvekst. Fra utvikling av prototyper til modernisering av eldre systemer, sikrer vi at våre kunder når sine mål. KodeWorks leverer høyt kvalifiserte teknologer som integreres sømløst i dine prosjekter for å skape langsiktige verdier.',
             },
             {
                 bilde: '/static/photos/tjenester/kodeworks-tjenester3.png',
-                tittel: 'Design',
-                tekst: 'Vi designer brukervennlige og estetisk tiltalende løsninger som',
+                tittel: 'Riktig Kompetanse',
+                tekst: 'KodeWorks leverer høyt kvalifiserte teknologer som integreres sømløst i kundens prosjekter. Kontakt oss for å lære mer om hvordan våre konsulenter kan hjelpe din virksomhet med å lykkes i et stadig mer komplekst teknologisk landskap.',
             },
         ],
     },
