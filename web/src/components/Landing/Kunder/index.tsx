9d13099c5350b04434c6c49ca3fa7dc8fd4825b2
Thomas Wold <thmwld@gmail.com>
2024-08-07 13:31:26 +0200

    center align customer logos on mobile

--- web/src/components/Landing/Kunder/index.tsx
+++ web/src/components/Landing/Kunder/index.tsx
@@ -6,7 +6,7 @@ const Kunder = () => {
   return (
     <div className="flex flex-col">
       <h2 className="section-title">Våre Kunder</h2>
-      <div className="flex flex-row flex-wrap items-center lg:justify-start justify-start xl:gap-x-12 xl:gap-y-12 lg:gap-x-6 lg:gap-y-6 gap-x-4 gap-y-4">
+      <div className="flex flex-row flex-wrap items-center sm:justify-start justify-center xl:gap-x-12 xl:gap-y-12 lg:gap-x-6 lg:gap-y-6 gap-x-4 gap-y-4">
         {logos.map((logo, index) => {
           return (
             <div key={index} className="logo">
