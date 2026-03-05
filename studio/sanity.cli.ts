47bf24a5384d3ddfb2308f114e584492e3f170d5
John Hagen <john@kodeworks.no>
2026-03-05 13:36:23 +0100

    Remove comment

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -9,7 +9,6 @@ export default defineCliConfig({
   },
   typegen: {
     path: "'../web/src/**/*.{ts,tsx,js,jsx}'", // glob pattern to typescript files that will be used in typegen
-    // path: "../web/src/lib/sanity.ts", // glob pattern to typescript files that will be used in typegen
     schema: './schema.json', // path to schema file, generated with 'sanity schema extract' command
     generates: '../web/src/types/sanity.types.ts', // path to the output file for generated type definitions
   },
